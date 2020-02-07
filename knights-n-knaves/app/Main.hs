6c5577325135c2320f1dbfa0e237db733b18d21b
Eben Kadile <eben.cowley42@gmail.com>
2020-02-07 16:51:32 -0500

    finished writing main loop, just need to test

--- knights-n-knaves/app/Main.hs
+++ knights-n-knaves/app/Main.hs
@@ -1,6 +1,37 @@
 module Main where
 
 import Statement
+import Saying
+import Parsing
+
+import Data.Word
+import Data.Bits
+import Data.List as L
+import Data.Vector as V
+
+showSoln :: Int -> Vector String -> Word64 -> String
+showSoln numCharacters names soln =
+  let knights = L.map (\i -> names!i) $ L.filter (testBit soln) [0..numCharacters]
+      knaves = L.map (\i -> names!i) $ L.filter (not . (testBit soln)) [0..numCharacters]
+  in "  Knights: " L.++ (L.intercalate ", " knights)
+       L.++ "\n  " L.++ "Knaves: " L.++ (L.intercalate ", " knights)
+
+showSolns :: Int -> Vector String -> [Word64] -> String
+showSolns numCharacters names solns =
+  let strs = L.map (showSoln numCharacters names) solns
+  in L.concat $ L.map (\(i, str) ->
+      "Solution " L.++ (show i) L.++ ":\n" L.++ str) $ L.zip [0..] strs
+
+takeInputAndCompute :: Vector String -> [Saying] -> String -> IO ()
+takeInputAndCompute names sayings input =
+  if input == "solve" then
+    let numCharacters = V.length names
+        solns         = findAllSolutions numCharacters sayings
+    in putStrLn $ showSolns numCharacters names solns
+  else
+    case parseSaying names input of
+      Nothing        -> putStrLn "Parse error!" >> getLine >>= takeInputAndCompute names sayings
+      Just (ns, say) -> getLine >>= takeInputAndCompute ns (say:sayings)
 
 main :: IO ()
-main = putStrLn "Hello World."
+main = getLine >>= takeInputAndCompute V.empty []
--- knights-n-knaves/src/Saying.hs
+++ knights-n-knaves/src/Saying.hs
@@ -2,6 +2,9 @@ module Saying where
 
 import Data.Word
 import Data.Bits
+import Data.List
+
+import Control.Parallel.Strategies
 
 import Statement
 
@@ -10,5 +13,13 @@ type Saying = (Int, Statement)
 checkSaying :: Word64 -> Saying -> Bool
 checkSaying w (i, s) = testBit (evalStatement w s) i
 
-checkSayings :: Word64 -> [Saying] -> Bool
-checkSayings w sayings = all (checkSaying w) sayings
+checkSayings :: [Saying] -> Word64 -> Bool
+checkSayings sayings w = all (checkSaying w) sayings
+
+findAllSolutions :: Int -> [Saying] -> [Word64]
+findAllSolutions numCharacters sayings =
+  if numCharacters > 2 then
+    let ncd8      = (2^numCharacters) `div` 8
+        allWorlds = map (\i -> [i*ncd8 .. (i + 1)*ncd8 - 1]) [0..7]
+    in concat $ parMap rpar (filter (checkSayings sayings)) allWorlds
+  else filter (checkSayings sayings) [0 .. 2^numCharacters - 1]
