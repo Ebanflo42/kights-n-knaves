19d769cb77cdacc43406ca7e8c77786bb082da0f
Eben Kadile <eben.cowley42@gmail.com>
2020-02-15 17:01:31 -0500

    improved user interface

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,8 @@ import Data.Bits
 import Data.List as L
 import Data.Vector as V
 
+import System.Console.Readline
+
 showSoln :: Int -> Vector String -> Word64 -> String
 showSoln numCharacters names soln =
   let knights = L.map (\i -> names!i) $ L.filter (testBit soln) [0 .. numCharacters - 1]
@@ -22,18 +24,22 @@ showSolns numCharacters names solns =
   in L.concat $ L.map (\(i, str) ->
       "\nSolution " L.++ (show i) L.++ ":\n" L.++ str) $ L.zip [1..] strs
 
-takeInputAndCompute :: Vector String -> [Saying] -> String -> IO ()
-takeInputAndCompute names sayings input =
+takeInputAndCompute :: Vector String -> [Saying] -> Maybe String -> IO ()
+takeInputAndCompute names sayings Nothing      =
+  readline "" >>= takeInputAndCompute names sayings
+takeInputAndCompute names sayings (Just input) =
   if input == "solve" then
     let numCharacters = V.length names
         solns         = findAllSolutions numCharacters $ refineSayings numCharacters sayings
     in putStrLn (showSolns numCharacters names solns)
-         >> getLine >>= takeInputAndCompute V.empty []
+         >> readline "" >>= takeInputAndCompute V.empty []
+  else if input == "clear" then
+    readline "Memory cleared.\n" >>= takeInputAndCompute V.empty []
   else if input == "quit" then return ()
   else
     case parseSaying names input of
-      Nothing        -> putStrLn "Parse error!" >> getLine >>= takeInputAndCompute names sayings
-      Just (ns, say) -> getLine >>= takeInputAndCompute ns (say:sayings)
+      Nothing        -> readline "Parse error!\n" >>= takeInputAndCompute names sayings
+      Just (ns, say) -> readline "" >>= takeInputAndCompute ns (say:sayings)
 
 main :: IO ()
-main = getLine >>= takeInputAndCompute V.empty []
+main = readline "Type \"quit\" to quit, \"solve\" to solve, and \"clear\" to clear all past inputs from memory.\nSolving the puzzle automatically clears memory.\nSee https://github.com/Ebanflo42/knights-n-knaves for the license and a usage example.\n\n" >>= takeInputAndCompute V.empty []
